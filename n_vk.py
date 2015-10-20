f5bb220ded490dd7076a9dc0b2aca0d10d04a47f
Dmytry Zelinsky <d.s.zelinsky@gmail.com>
2015-10-20 22:00:18 +0300

    Manik

--- n_vk.py
+++ n_vk.py
@@ -3,7 +3,9 @@ import sys
 import vk
 import time
 import os
-
+###
+adasd
+###
 vkapi = vk.API('4597440', 'Your login','Your password',scope=2097151) #Takes token from vk.com
 s = vkapi.access_token 
 while True:
